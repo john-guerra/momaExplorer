8da7ee4c3f264675fda36b1f9d255ad9d048f222
John Alexis Guerra Gómez <john.guerra@gmail.com>
2023-07-24 17:54:35 -0700

    Updates

---
+++ precache-manifest.24ef1b8ea952b6d159d27a91f74877ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9f01867c78ab93f443bc",
+    "url": "./static/css/main.36202cd3.chunk.css"
+  },
+  {
+    "revision": "9f01867c78ab93f443bc",
+    "url": "./static/js/main.92aa0d48.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "b74e52ebc97911037e54",
+    "url": "./static/js/2.21e59fe0.chunk.js"
+  },
+  {
+    "revision": "3d83bb93689b06179cf0437c86306c90",
+    "url": "./index.html"
+  }
+];
